f76904d26dcf82204f9104e03f3c27ef596aa28b
David <dachovi@outlook.com>
2020-11-01 00:13:29 -0300

    3er cambio composer

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'fabricadeempanadaslamascada.cl'),
 
     'asset_url' => env('ASSET_URL', null),
 
